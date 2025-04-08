583c8aa74ab6476aca16ad53489cfd8947488a5c
luqmanbeast <luqman.hakim321@gmail.com>
2025-04-08 17:37:59 +0800

    push
    we

---
+++ script.js
@@ -0,0 +1,90 @@
+// let url = 'F:/WebsitePortfolio_Luccman/122130385_2658_AmendedFinalReport.pdf'; // Replace with the path to your PDF
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.5,
+      canvas = document.querySelector('#pdf-render'),
+      ctx = canvas.getContext('2d');
+
+      
+// Render the page
+const renderPage = num => {
+    pageIsRendering = true;
+
+    // Get page
+    pdfDoc.getPage(num).then(page => {
+        const viewport = page.getViewport({ scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        };
+
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false;
+
+            if (pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        });
+
+        // Output current page
+        document.querySelector('#page-num').textContent = num;
+    });
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+    if (pageIsRendering) {
+        pageNumIsPending = num;
+    } else {
+        renderPage(num);
+    }
+};
+
+// Show Prev page
+document.querySelector('#prev-page').addEventListener('click', () => {
+    if (pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+});
+
+// Show Next page
+document.querySelector('#next-page').addEventListener('click', () => {
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+});
+
+// Get Document
+// pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+//     pdfDoc = pdfDoc_;
+//     document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+//     renderPage(pageNum);
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // For smooth scrolling
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+
